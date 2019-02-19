3bd4a3303a3f297881fce6078616ebf3666877d4
unknown <you@example.com>
2019-02-19 11:31:28 +0000

    Added end to end testing and changes to Readme

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.1.
 
+It's used as the frontend to work with the RESTful-API ramverk2, for the course "ramverk2", which is a course at BTH with focus on web frameworks.
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
@@ -18,9 +20,17 @@ Run `ng build` to build the project. The build artifacts will be stored in the `
 
 Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
 
+No tests yet.
+
 ## Running end-to-end tests
 
 Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
+
+### Use cases tested
+
+1. Open up the homepage and navigate to /reports using the navbar.
+2. From the reportspage, go to `kmom01` using the button on the page.
+3. Try to log in using a email that do not exist, should result in a message saying that user is not found.
 
 ## Further help
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,23 +1,46 @@
-import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, by, element } from 'protractor';
 
-describe('workspace-project App', () => {
-  let page: AppPage;
+describe('End to end testing', function () {
+  it('Should navigate to reports from nav-bar', function () {
+    browser.get('http://localhost:4200/');
+    browser.driver.manage().window().maximize();
 
-  beforeEach(() => {
-    page = new AppPage();
+    let allLinks = element.all(by.css('a'))
+    expect(allLinks.count()).toEqual(4)
+    expect(allLinks.get(2).getText()).toEqual("Redovisa")
+
+    allLinks.get(2).click();
+
+    expect(browser.getCurrentUrl()).toEqual("http://localhost:4200/reports");
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to my-app!');
+  it('Should go to Kmom01 using buttons on the screen', function () {
+    browser.get('http://localhost:4200/reports');
+    browser.driver.manage().window().maximize();
+    let kmom01 = element(by.cssContainingText(".button", 'Kmom01'));
+
+    expect(kmom01.getText()).toEqual("Kmom01")
+
+    kmom01.click();
+
+    expect(browser.getCurrentUrl()).toEqual("http://localhost:4200/reports/kmom01");
   });
 
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    }));
+  it('Should show User not found, when trying to login.', function () {
+    browser.get('http://localhost:4200/login');
+    browser.driver.manage().window().maximize();
+
+    let emailInput = element(by.id("email"));
+    let passwordInput = element(by.id("password"));
+    let submit = element(by.css("button"));
+
+    emailInput.sendKeys("wontwork");
+    passwordInput.sendKeys("wrongpassowrd");
+    submit.click();
+
+    let message = element(by.css(".is-danger"));
+
+    expect(message.getText()).toEqual("User not found");
+
   });
 });
