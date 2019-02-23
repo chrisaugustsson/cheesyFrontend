ef293bfa00fb95d73d56af91ddc27d3415b36c5c
unknown <you@example.com>
2019-02-23 16:45:07 +0000

    Changed URL to backend

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -43,7 +43,7 @@ export class ChatService {
   }
 
   connect(user) {
-    this.socket = io("http://localhost:1337", {
+    this.socket = io("https://me-api.chau17.me", {
       query: {
         user: user
       }
